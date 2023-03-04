d27ad8bd51a533af8ca0a0e5909d6f61a66e42d2
devZ_VikAsH <pvickey663@gmail.com>
2023-03-04 12:49:27 +0530

    majority > arr.length/3

--- MajorityElem.java
+++ MajorityElem.java
@@ -19,8 +19,24 @@ public class MajorityElem {
         return curr;
     }
 
+    public static void majorityBy3(int arr[]) {
+
+        int temp[] = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            temp[arr[i]]=temp[arr[i]]+1;
+        }
+
+        for( int i=0; i<temp.length; i++){
+            if(temp[i]>arr.length/3){
+                System.out.println(temp[i]+ "=>"+ i);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 5, 5, 5, 4, 4, 4, 4 };
-        System.out.println(findMajority(arr));
+        int arr[] = { 5, 5, 3, 4, 4, 4, 4 };
+        // System.out.println(findMajority(arr));
+        majorityBy3(arr);
     }
 }
